1de39eeca6c227ea670585e7468451be1227dfb2
Clayton Lopes <junin-lopes-2008@hotmail.com>
2022-10-13 12:27:24 -0300

    add database connection and universities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UniversitiesModule } from './universities/universities.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/db'),
+    UniversitiesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
